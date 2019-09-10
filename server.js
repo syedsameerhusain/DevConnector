d65789fbf32d3e6df5ab5f6e1ee56d76802f2ef9
Syed Sameer Husain <husainsam432@gmail.com>
2019-09-10 19:57:04 +0530

    hello commit done

--- server.js
+++ server.js
@@ -2,4 +2,4 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
 app.get('/', (req, res) => res.send('api running'));
-app.listen(PORT, () => console.log(`server started on port ${PORT}`));
+app.listen(PORT, () => console.log(`server started on port number ${PORT}`));
