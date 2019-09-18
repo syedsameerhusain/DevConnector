32d5ba8256d8d6bab7783b79ae3858fa5e0f9a58
Syed Sameer Husain <husainsam432@gmail.com>
2019-09-18 16:59:43 +0530

    Many changes occured

--- config/db.js
+++ config/db.js
@@ -5,12 +5,14 @@ const db = config.get('mongoURI');
 const connectDB = async () => {
   try {
     await mongoose.connect(db, {
-      useNewUrlParser: true
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false
     });
     console.log('mongodb connected...');
   } catch (err) {
     console.error(err.message);
-    //exit
+    //exit ....
     process.exit(1);
   }
 };
--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express');
-// const connectDB = require('./config/db');
+const connectDB = require('./config/db');
 const app = express();
 //connect db
-// connectDB();
+connectDB();
+
+//Init Middware
+app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => res.send('API Running'));
 const PORT = process.env.PORT || 5000;
+//define routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
-app.listen(PORT, () => console.log(`server started on port number ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port  ${PORT}`));
