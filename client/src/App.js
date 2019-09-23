72b6865a48f84fcb5850b327987a913484097010
Syed Sameer Husain <husainsam432@gmail.com>
2019-09-23 20:41:22 +0530

    React front end started and redux set with alerts

---
+++ client/src/App.js
@@ -0,0 +1,31 @@
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/layouts/Navbar';
+import Landing from './components/layouts/Landing';
+import Login from './components/auth/login';
+import Register from './components/auth/Register';
+import './App.css';
+import Alert from './components/layouts/Alert';
+
+//Redux
+import { Provider } from 'react-redux';
+import store from './store';
+const App = () => (
+  <Provider store={store}>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Route exact path='/' component={Landing} />
+        <section className='container'>
+          <Alert />
+          <Switch>
+            <Route exact path='/register' component={Register} />
+            <Route exact path='/login' component={Login} />
+          </Switch>
+        </section>
+      </Fragment>
+    </Router>
+  </Provider>
+);
+
+export default App;
